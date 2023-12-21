a872970905a312483ab7aa0e3c2903a201f87102
Mark Mulvaney <mark@bambwa.com>
2023-12-21 17:47:51 +0100

    changed stega details

--- app/sanity/projectDetails.ts
+++ app/sanity/projectDetails.ts
@@ -6,12 +6,12 @@
 declare global {
   interface Window {
     ENV: {
-      SANITY_STUDIO_PROJECT_ID: string
-      SANITY_STUDIO_DATASET: string
-      SANITY_STUDIO_API_VERSION: string
-      SANITY_FRONTEND_URL: string
-      SANITY_STUDIO_URL: string
-    }
+      SANITY_STUDIO_PROJECT_ID: string;
+      SANITY_STUDIO_DATASET: string;
+      SANITY_STUDIO_API_VERSION: string;
+      SANITY_FRONTEND_URL: string;
+      SANITY_STUDIO_URL: string;
+    };
   }
 }
 
@@ -19,17 +19,17 @@ const {
   SANITY_STUDIO_PROJECT_ID,
   SANITY_STUDIO_DATASET,
   SANITY_STUDIO_API_VERSION,
-} = typeof document === 'undefined' ? process.env : window.ENV
+} = typeof document === 'undefined' ? process.env : window.ENV;
 
-export const projectId = SANITY_STUDIO_PROJECT_ID!
-export const dataset = SANITY_STUDIO_DATASET!
-export const apiVersion = SANITY_STUDIO_API_VERSION!
+export const projectId = SANITY_STUDIO_PROJECT_ID!;
+export const dataset = SANITY_STUDIO_DATASET!;
+export const apiVersion = SANITY_STUDIO_API_VERSION!;
 
 export const projectDetails = () => ({
   projectId,
   dataset,
   apiVersion,
-})
+});
 
 // Enable stega on production deploys, but NOT the non-production domain
 // Allow the production Studio to access non-production domains cross-origin
@@ -41,7 +41,7 @@ export const projectDetails = () => ({
 
 // TODO: Replace with YOUR production domain
 // This is used to enable stega on any URL except this one
-export const PRODUCTION_URL = 'https://sanity-remix-template.sanity.build'
+export const PRODUCTION_URL = 'https://ddas-manimul.vercel.app/';
 
 // With the logic below we enable stega only on the non-production domain
 export const frontendUrl =
@@ -49,7 +49,7 @@ export const frontendUrl =
     ? process.env.VERCEL
       ? `https://${process.env.VERCEL_BRANCH_URL}`
       : process.env.SANITY_FRONTEND_URL!
-    : window.ENV.SANITY_FRONTEND_URL!
+    : window.ENV.SANITY_FRONTEND_URL!;
 
 export const studioUrl =
   typeof document === 'undefined'
@@ -58,14 +58,14 @@ export const studioUrl =
         ? `https://${process.env.VERCEL_URL}`
         : PRODUCTION_URL
       : process.env.SANITY_STUDIO_URL!
-    : window.ENV.SANITY_STUDIO_URL!
+    : window.ENV.SANITY_STUDIO_URL!;
 
 export const stegaEnabled =
-  new URL(studioUrl).hostname !== new URL(PRODUCTION_URL).hostname
+  new URL(studioUrl).hostname !== new URL(PRODUCTION_URL).hostname;
 
 // If any of these values are missing, throw errors as the app requires them
-if (!projectId) throw new Error('Missing SANITY_STUDIO_PROJECT_ID in .env')
-if (!dataset) throw new Error('Missing SANITY_STUDIO_DATASET in .env')
-if (!apiVersion) throw new Error('Missing SANITY_STUDIO_API_VERSION in .env')
-if (!frontendUrl) throw new Error('Missing SANITY_FRONTEND_URL in root route')
-if (!studioUrl) throw new Error('Missing SANITY_STUDIO_URL in root route')
+if (!projectId) throw new Error('Missing SANITY_STUDIO_PROJECT_ID in .env');
+if (!dataset) throw new Error('Missing SANITY_STUDIO_DATASET in .env');
+if (!apiVersion) throw new Error('Missing SANITY_STUDIO_API_VERSION in .env');
+if (!frontendUrl) throw new Error('Missing SANITY_FRONTEND_URL in root route');
+if (!studioUrl) throw new Error('Missing SANITY_STUDIO_URL in root route');
