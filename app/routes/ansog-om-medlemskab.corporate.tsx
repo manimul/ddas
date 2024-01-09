6d398a291c1ded91213e741a22e9eaa41f4e5c4f
Mark Mulvaney <mark@bambwa.com>
2024-01-09 14:19:30 +0100

    updated forms

--- app/routes/ansog-om-medlemskab.corporate.tsx
+++ app/routes/ansog-om-medlemskab.corporate.tsx
@@ -1,7 +1,161 @@
+import { Form } from '@remix-run/react';
+import { z } from 'zod';
+//import { Form } from '~/components/Form';
+
+const schema = z.object({
+  firstName: z.string().min(1),
+  email: z.string().min(1).email(),
+  //howYouFoundOutAboutUs: z.enum(['fromAFriend', 'google']),
+});
+
 export default function Corporate() {
+  const getYears = () => {
+    const currentYear = new Date().getFullYear();
+    return Array.from({ length: 100 }, (v, i) => currentYear - i);
+  };
+
   return (
-    <div className='grid grid-cols-1 border-orange-500 border-2  gap-6 lg:gap-12'>
-      Corporate
+    <div className='grid md:grid-cols-8 gap-6 '>
+      <div className=' col-span-4 space-y-4'>
+        <h1 className='text-4xl '>Corporate Medlemskap</h1>
+        <div className='prose font-serif dark:prose-invert lg:prose-lg  prose-a:text-cyan-600 dark:prose-a:text-cyan-200'>
+          Det Danske Afrika Selskab giver mulighed for at firmaer med
+          aktiviteter i Afrika, eller som har særlige interesser på kontinentet,
+          kan blive optaget som Corporate Member. For medlemskabet gælder at: Op
+          til fire medarbejdere fra firmaet kan deltage i hvert af selskabets
+          arrangementer. Deltagelse sker på samme fordelagtige vilkår og priser
+          som for selskabets øvrige medlemmer. Det årlige medlemskab er 5000 kr.
+        </div>
+      </div>
+      <Form method='post' className='col-span-4  max-w-lg mx-auto  '>
+        <div className='flex flex-wrap -mx-3 mb-6'>
+          <div className='w-full  px-3 mb-6 md:mb-0'>
+            <label
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='grid-first-name'
+            >
+              Navn
+            </label>
+            <input
+              className='appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+              id='navn'
+              type='text'
+              placeholder='Jane Doe'
+            />
+          </div>
+
+          <div className='w-full  px-3 mb-6 md:mb-0'>
+            <label
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='grid-first-name'
+            >
+              Adresse
+            </label>
+            <input
+              className='appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+              id='addresse'
+              type='text'
+              placeholder='Adresse 123           '
+            />
+          </div>
+
+          <div className='w-full  px-3 mb-6 md:mb-0'>
+            <label
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='nummer'
+            >
+              Telefonnummer
+            </label>
+            <input
+              className='appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+              id='nummer'
+              type='number'
+              placeholder='004523456789'
+            />
+          </div>
+
+          <div className='w-full  px-3 mb-6 md:mb-0'>
+            <label
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='postnummer'
+            >
+              Postnummer og by{' '}
+            </label>
+            <input
+              className='appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+              id='postnummer'
+              type='number'
+              placeholder='00452'
+            />
+          </div>
+
+          <div className='w-full  px-3 mb-6 md:mb-0'>
+            <label
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='email'
+            >
+              Email
+            </label>
+            <input
+              className='appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+              id='email'
+              type='email'
+              placeholder='yourname@email.com'
+            />
+          </div>
+
+          <div className='w-full px-3 mb-6 md:mb-0'>
+            <label
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='year-select'
+            >
+              Fødselsår
+            </label>
+            <div className='relative'>
+              <select
+                className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 mb-3 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                id='year-select'
+              >
+                {getYears().map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                ))}
+              </select>
+              <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+                <svg
+                  className='fill-current h-4 w-4'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 20 20'
+                >
+                  <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className='w-full  px-3 mb-6 md:mb-0'>
+            <label
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='email'
+            >
+              Beskrivelse af din Afrikaerfaring:
+            </label>
+            <textarea
+              className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+              id='year'
+            />
+          </div>
+        </div>
+
+        <div className='flex items-center justify-between'>
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+            type='button'
+          >
+            Send
+          </button>
+        </div>
+      </Form>
     </div>
   );
 }
