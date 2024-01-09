4b57ea203b49721d63138bd88c5e8df12ab54bfd
Mark Mulvaney <mark@bambwa.com>
2024-01-09 16:42:53 +0100

    updated nav for mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,8 +3,11 @@ import type { LayoutProps } from '~/components/Layout';
 import { Logo } from '~/components/Logo';
 import { ThemeToggle } from '~/components/ThemeToggle';
 import Dropdown from '~/components/Dropdown';
+import { useState } from 'react';
 
 export function Header(props: LayoutProps) {
+  const [navbar, setNavbar] = useState(false);
+
   const omOssItems = [
     { slug: '/om-oss', text: 'Om Oss' },
     { slug: '/om-oss/bestyrelsen', text: 'Bestyrelsen' },
@@ -27,10 +30,10 @@ export function Header(props: LayoutProps) {
 
   return (
     <header className='border-b border-gray-100 transition-colors duration-1000 ease-in-out dark:border-gray-900'>
-      <div className='container mx-auto flex flex-col md:flex-row items-center justify-between p-4 lg:px-12'>
+      <div className='container mx-auto flex  md:flex-row items-center justify-between p-4 lg:px-12'>
         <Logo home={props.home} />
 
-        <div className='mx-auto  items-center justify-between md:p-4 lg:px-4   md:flex grow'>
+        <div className='hidden  mx-auto  items-center justify-between md:p-4 lg:px-4   md:flex grow'>
           <Dropdown title='Om' items={omOssItems} />
           <Dropdown title='Medlemshjørnet' items={medlemItems} />
 
@@ -52,10 +55,91 @@ export function Header(props: LayoutProps) {
           >
             Ansøg om medlemskap{' '}
           </Link>
+          <ThemeToggle theme={props.theme} />
         </div>
 
-        <ThemeToggle theme={props.theme} />
+        <div className='md:hidden mx-auto  items-center justify-between md:p-4 lg:px-4   flex grow'>
+          <button
+            className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
+            onClick={() => setNavbar(!navbar)}
+          >
+            {navbar ? (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='w-6 h-6'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+                  clipRule='evenodd'
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='w-6 h-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M4 6h16M4 12h16M4 18h16'
+                />
+              </svg>
+            )}
+          </button>
+        </div>
       </div>
+      {navbar && (
+        <div
+          className={`flex-1 justify-between min-h-screen md:block md:pb-0 md:mt-0 ${
+            navbar ? 'block' : 'hidden'
+          }`}
+        >
+          <div className='flex flex-col text-center grow h-auto  '>
+            <Dropdown title='Om' items={omOssItems} />
+            <Dropdown title='Medlemshjørnet' items={medlemItems} />
+
+            <Link
+              className='py-3'
+              to='arrangementer'
+              onClick={() => setNavbar(!navbar)}
+            >
+              Arrangementer{' '}
+            </Link>
+            <Link
+              to='nyheder'
+              className='py-3'
+              onClick={() => setNavbar(!navbar)}
+            >
+              Nyheder{' '}
+            </Link>
+            <Link
+              to='kontakt'
+              className='py-3'
+              onClick={() => setNavbar(!navbar)}
+            >
+              Kontakt{' '}
+            </Link>
+            <Link to='' className='py-3' onClick={() => setNavbar(!navbar)}>
+              Mer{' '}
+            </Link>
+            <Link
+              to='ansog-om-medlemskab'
+              className='  py-3 '
+              onClick={() => setNavbar(!navbar)}
+            >
+              Ansøg om medlemskap{' '}
+            </Link>
+            <ThemeToggle theme={props.theme} />
+          </div>
+        </div>
+      )}
     </header>
   );
 }
